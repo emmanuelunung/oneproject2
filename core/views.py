1f912fde0526ff542254318943d1fb9d1ed12297
emmanuelunung <eunung2003@gmail.com>
2024-11-02 14:33:15 +0100

    updated settings

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.db.models import Q
 from .models import Product
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
